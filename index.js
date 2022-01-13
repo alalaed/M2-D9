e79d2742396c00f3076defea7f4fc0481cd87a22
alalaed <a.alaeddine@live.com>
2022-01-13 16:42:08 +0100

    ex7

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 
 
 
-const deleteRow = function(){
-    let chosenRow = document.getElementsByTagName("tr")
-    
+const addRow = function(){
+    let table = document.querySelector("tbody")
+    const inhalt = `
+    <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>the Bird</td>
+            <td><button type="button" class="btn btn-danger">Delete</button></td>
+          </tr>
+        `
+
+    table.innerHTML+=inhalt
+
 }
 
 
