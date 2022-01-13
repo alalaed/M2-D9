9cea78b6a57e3eea47168be833aed7d0e2f25afb
alalaed <a.alaeddine@live.com>
2022-01-13 15:00:11 +0100

    Ex4

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+// const genres = ["HipHop", "Rap", "Jazz", "Alt.Rock", "RnB", "Pop"]
+
+
+
+
+// let list = document.createElement("ul")
+// let bodyContainer= document.querySelector("body")
+// bodyContainer.appendChild(list)
+
+// for( i=0; i<genres.length; i++){
+//     let item = document.createElement("li")
+//     item.innerText=genres[i]
+//     list.appendChild(item)
+// }
+
+
+
+
+
+
+
+
